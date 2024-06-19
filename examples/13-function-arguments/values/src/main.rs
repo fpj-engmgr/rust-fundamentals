dab8d8b7bffdb01909ed285d4c4f69b850915598
Frank Jansen <fphjansen@gmail.com>
2024-06-19 19:49:38 -0400

    added user input to 13-function-arguments-values

--- examples/13-function-arguments/values/src/main.rs
+++ examples/13-function-arguments/values/src/main.rs
@@ -9,7 +9,14 @@ fn sum(numbers: &[i32]) -> i32 {
 
 fn main() {
     // There are no variadic arguments in Rust
-    let numbers = [1, 2, 3, 4, 5];
+    use std::io::{stdin, stdout, Write};
+    let mut sinput = String::new();
+    print!("Enter a list of numbers separated by spaces: ");
+    let _ = stdout().flush();
+    stdin().read_line(&mut sinput).expect("Failed to read line");
+    let numbers: Vec<i32> = sinput.split_whitespace() // Split the input string into a vector of words
+        .map(|s| s.parse().expect("parse error")) // Convert each word into a number
+        .collect(); // Collect the numbers into a vector
     let result = sum(&numbers);
     println!("The sum is {}", result);
 }
